890aca77afe12101205a564463e5ce1442dd249f
NicholasSchenck <nschenck@Nicholas-Schencks-MacBook-Air.local>
2015-10-25 17:14:09 -0700

    Ummmm
    Submitting a clueless assignemtn

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Not sure what this assignement is looking for
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This is a emppty cacheSolve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
